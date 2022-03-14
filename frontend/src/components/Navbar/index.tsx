5f7c75855c8fd0b04f5bb5794080178b630ca54b
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2022-03-13 22:55:36 -0300

    Atualizacao

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ function Navbar() {
                     <a href="https://github.com/devsuperior">
                         <div className = "dsmovie-contact-container">
                             <GithubIcon />
-                            <p className = "dsmovie-contact-link">/devsuperior</p>
+                            <p className = "dsmovie-contact-link">/antoniocarlos-bc</p>
                         </div>
                     </a>
                 </div>
